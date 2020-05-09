9acdfe8e3d53b7670bd06d569313893b12946bfa
Gareth Nunns <garethnunns@gmail.com>
2020-05-09 01:20:34 +0100

    Base design

--- App.js
+++ App.js
@@ -1,26 +1,77 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, ScrollView } from 'react-native';
 
-import { useFonts } from '@use-expo/font';
-import { AppLoading } from 'expo';
+import { DataTable, Switch, TextInput, Appbar, DarkTheme, Provider as PaperProvider } from 'react-native-paper';
 
-import { Button, Text } from 'react-native-material-ui';
+const theme = {
+  ...DarkTheme,
+  colors: {
+    ...DarkTheme.colors,
+    primary: '#2196f3'
+  }
+};
 
 export default function App() {
-  let [fontsLoaded] = useFonts({
-    'Roboto': require('./assets/fonts/Roboto-Regular.ttf')
-  });
+  return (
+    <PaperProvider theme={theme}>
+      <Appbar.Header>
+        <Appbar.Content
+          title="EM Calc"
+        />
+        <Appbar.Action icon="dots-vertical" />
+      </Appbar.Header>
+      <ScrollView style={{backgroundColor:theme.colors.surface}}>
+        <TextInput
+          label='Horizontal Pixels'
+          mode='outlined'
+          keyboardType={'numeric'}
+        />
+        <TextInput
+          label='Vertical Pixels'
+          mode='outlined'
+          keyboardType={'numeric'}
+        />
+        <TextInput
+          label='Refresh Rate'
+          mode='outlined'
+          keyboardType={'numeric'}
+        />
+        <Switch
+        />
 
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  } else {
-    return (
-      <View style={styles.container}>
-        <Text>E2 Calc</Text>
-        <Button primary raised={true} text="Primary" />
-      </View>
-    );
-  }
+        <DataTable>
+          <DataTable.Header>
+            <DataTable.Title>EDID Timing</DataTable.Title>
+          </DataTable.Header>
+
+          <DataTable.Row>
+            <DataTable.Cell>H Total</DataTable.Cell>
+            <DataTable.Cell numeric>2080</DataTable.Cell>
+          </DataTable.Row>
+
+          <DataTable.Row>
+            <DataTable.Cell>H Front Porch</DataTable.Cell>
+            <DataTable.Cell numeric>48</DataTable.Cell>
+          </DataTable.Row>
+
+          <DataTable.Row>
+            <DataTable.Cell>H Active</DataTable.Cell>
+            <DataTable.Cell numeric>1920</DataTable.Cell>
+          </DataTable.Row>
+
+          <DataTable.Row>
+            <DataTable.Cell>H Sync</DataTable.Cell>
+            <DataTable.Cell numeric>32</DataTable.Cell>
+          </DataTable.Row>
+
+          <DataTable.Row>
+            <DataTable.Cell>H Polarity</DataTable.Cell>
+            <DataTable.Cell numeric>32</DataTable.Cell>
+          </DataTable.Row>
+        </DataTable>
+      </ScrollView>
+    </PaperProvider>
+  );
 }
 
 const styles = StyleSheet.create({
