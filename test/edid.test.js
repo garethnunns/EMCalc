052f49a0a06c917675b956c687a7a77ee1461b40
Gareth Nunns <garethnunns@gmail.com>
2020-05-10 12:48:10 +0100

    Calculate EDID test

--- test/edid.test.js
+++ test/edid.test.js
@@ -2,13 +2,42 @@ const assert = require('assert');
 const edid = require('./../src/lib/edid')
 
 describe('Constuctor', () => {
-  const constEdid = new edid(1920,1080,60,true,2)
+  const constEdid = new edid(1920,1080,60,false,true,2)
 
-  it('should set horizontal pixels', () => {
-    assert.equal(constEdid.params.hor, 1920);
+  it('should set params', () => {
+    assert.equal(constEdid.params.hPx, 1920)
+    assert.equal(constEdid.params.vPx, 1080);
+    assert.equal(constEdid.params.refresh, 60);
+    assert.equal(constEdid.params.margins, false);
+    assert.equal(constEdid.params.redBlnk, true);
+    assert.equal(constEdid.params.redBlnkV, 2);
   });
-  it('should set vertical pixels', () => {
-    assert.equal(constEdid.params.ver, 1080);
+});
+
+describe('Calculate EDID', () => {
+  it('1920x1080@60 // no margins // no reduced blanking', () => {
+    const calcEdid = new edid(1920,1080,60,false,false,2)
+
+    var result = calcEdid.calculateEdid()
+    
+    console.log(result)
+
+    assert.equal(result.hTotalPx, 2578);
+    assert.equal(result.hFrontPorch, 123);
+    assert.equal(result.hActive, 1920);
+    assert.equal(result.hSync, 206);
+    assert.equal(result.hPolarity, false);
+
+    assert.equal(result.vTotalLines, 1120);
+    assert.equal(result.vFrontPorch, 3);
+    assert.equal(result.vActive, 1080);
+    assert.equal(result.vPolarity, true);
+    assert.equal(result.vSync, 5);
+    assert.equal(result.vRate, 60);
+    
+  });
+});
+
 describe('Aspect Ratio', () => {
   const aspectEdid = new edid(1920,1080,60,true,2)
 
