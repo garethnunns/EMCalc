8d5cab7a7313ab4e97d375113c8099e392f86e2b
Gareth Nunns <garethnunns@gmail.com>
2020-05-10 23:24:52 +0100

    Calculate EDID without reduced blanking

--- src/lib/edid.js
+++ src/lib/edid.js
@@ -1,8 +1,15 @@
 class edid {
+  /**
+   * Written by Gareth Nunns
+   * Inspired by the spreadsheet: https://www.facebook.com/groups/Barcofolsom/permalink/2524750314238262/
+   * Spreadsheet credits:
+   * Graham Loveridge & Syed Athar Hussain
+   */
+
   // standard timings
   timings = {
     // top & bottom margin
-    marginSize: 1.8, // (%)
+    marginSize: 0.018, // (1.8%)
 
     // cell horizontal granularity for each reduced blanking version
     hCellGranularity: {
@@ -18,12 +25,12 @@ class edid {
       '5:4': 7,
       '15:9': 7,
       'CVT-RB V2': 8,
-      'Reserved': 9,
-      'Custom': 10
+      'reserved': 9,
+      'custom': 10
     },
 
     // H Sync width
-    hSyncWidth: 8, // (%)
+    hSyncWidth: 0.08, // (8%)
 
     // analog vertical blanking limitations
     vMinSyncInterval: 550, // (μs)
@@ -35,9 +42,6 @@ class edid {
     hOffset: 40, // (%)
     hBlankingTimeScalingFactor: 128,
     hScalingFactorWeighting: 20,
-
-    //TODO
-    hGradientDash: this.timings.hGradient + this.timings.hOffset,
 
     // reduced blanking timing
     hBlanking: { // for each reduced blanking version
@@ -57,26 +61,173 @@ class edid {
     clockStep: { // for each reduced blanking version
       1: .25,
       2: .001
-    }
+    },
+    interlaced: false
   }
 
   /**
    * Initialise edid class with desired properties
-   * @param {number} hor - active horizontal pixels
-   * @param {number} ver - active vertical pixels
+   * @param {number} hPx - active horizontal pixels
+   * @param {number} vPx - active vertical pixels
    * @param {number} refresh - refresh rate
-   * @param {boolean=true} (optional) redBlnk - reduce blanking
-   * @param {number=2} (optional) redBlnkV - reduce blanking version
+   * @param {boolean=false} (optional) margins - if margins are required
+   * @param {boolean=true} (optional) redBlnk - if reduced blanking is required
+   * @param {number=2} (optional) redBlnkV - reduce blanking version to use
    */
-  constructor(hor, ver, refresh, redBlnk = true, redBlnkV = 2) {
+  constructor(hPx, vPx, refresh, margins = false, redBlnk = true, redBlnkV = 2) {
+    // calculate timings
+    this.timings.hGradientPrime = this.timings.hBlankingTimeScalingFactor / 256 * this.timings.hGradient
+    this.timings.hOffsetPrime = 
+      ( (this.timings.hOffset - this.timings.hScalingFactorWeighting) * this.timings.hBlankingTimeScalingFactor / 256 ) + this.timings.hScalingFactorWeighting
+    this.timings.vMinPorchRounded = Math.floor(this.timings.vMinPorch)
+
+
     this.params = {
-      hor: hor,
-      ver: ver,
+      hPx: hPx,
+      vPx: vPx,
+      margins: margins,
       refresh: refresh,
       redBlnk: redBlnk,
       redBlnkV: redBlnkV
     }
   }
+
+  calcEdid(params = this.params) {
+    // horizontal cell granularity
+    var hCellGranularityRounded = Math.floor(this.timings.hCellGranularity[params.redBlnkV])
+
+    // horizontal pixels
+    var hPxRounded = Math.floor(params.hPx / hCellGranularityRounded) * hCellGranularityRounded
+
+    // left and right margins
+    var hMargin = 0
+    if(params.margins) {
+      hMargin = Math.floor(hPxRounded * this.timings.marginSize / hCellGranularityRounded) * hCellGranularityRounded
+    }
+    
+    // total acitve horizontal pixels
+    const hActivePx = hPxRounded + 2 * hMargin
+
+    // required vertical field rate
+    var vFieldRate = params.refresh
+    if(this.timings.interlaced) {
+      vFieldRate *= 2
+    }
+
+    // vertical lines per field
+    var vLines = Math.floor(params.vPx)
+    if(this.timings.interlaced) {
+      vLines = Math.floor(params.vPx/2)
+    }
+
+    // top and bottom margins
+    var vMargin = 0
+    if(params.margins) {
+      vMargin = Math.floor(this.timings.marginSize * vLines)
+    }
+
+    // interlace factor
+    const interlace = this.timings.interlaced ? 0.5 : 0
+
+    if(!params.redBlnk) {
+      // no reduced blanking
+
+      // estimate horizontal period (μs)
+      const hEstPeriod = ((1 / vFieldRate) - this.timings.vMinSyncInterval / 1000000) / (vLines + 2 * vMargin + this.timings.vMinPorchRounded + interlace) * 1000000
+
+      // estimate num vertical lines
+      const vEstSyncBackPorch = Math.floor(this.timings.vMinSyncInterval / hEstPeriod) + 1
+
+      // num vertical lines
+      const vSyncBackPorch = Math.min(this.timings.vMinSyncInterval + this.timings.vMinBackPorch, vEstSyncBackPorch)
+
+      // aspect ratio
+      var aspect
+      if(params.redBlnk && redBlnkV) {
+        aspect = 'CVT-RB V2'
+      }
+      else {
+        aspect = this.aspectRatio(params.hPx,params.vPx)
+      }
+
+      const vSync = this.timings.vSyncWidth[aspect]
+
+      // lines in back porch
+      const vBackPorch = vSyncBackPorch - this.timings.vSyncWidth[aspect]
+
+      // total lines in vertical field
+      var vTotal = vLines + 2 * vMargin + vSyncBackPorch + interlace + this.timings.vMinPorchRounded
+      if(this.timings.interlaced) {
+        vTotal *= 2
+      }
+
+      // ideal blanking duty cycle (%)
+      const idealBlankingCycle = this.timings.hOffsetPrime - (this.timings.hGradientPrime * hEstPeriod / 1000)
+
+      var hBlankingTime
+      // find blanking time to nearest char cell
+      if(idealBlankingCycle < 20) {
+        hBlankingTime = Math.floor( (hActivePx * 20 / (100-20) / (2 * hCellGranularityRounded) ) ) * (2 * hCellGranularityRounded)
+      }
+      else {
+        hBlankingTime = Math.floor(hActivePx * idealBlankingCycle / (100 - idealBlankingCycle) / (2 * hCellGranularityRounded) ) * (2 * hCellGranularityRounded)
+      }
+
+      // total horizontal pixels in lines
+      const hTotal = hActivePx + hBlankingTime
+
+      // pixel clock frequency
+      const freq = hTotal / hEstPeriod
+
+      // horizontal sync
+      const hSyncRounded = Math.floor( this.timings.hSyncWidth * hTotal / hCellGranularityRounded ) * hCellGranularityRounded
+
+      // vertical front porch
+      const vFrontPorch = this.timings.vMinPorchRounded + interlace
+
+      // process independent
+
+      // barco links
+      const links = this.calcLinks(params.hPx, freq)
+
+      // horizontal back porch
+      const hBackPorch = hBlankingTime / 2
+
+      // horizontal front porch
+      const hFrontPorch = hBlankingTime - hBackPorch - hSyncRounded
+
+      // horizontal sync polarity
+      const hPolarity = params.redBlnk
+
+      // vertical active lines correct interlaced
+      if(this.timings.interlaced) {
+        vLines *= 2
+      }
+
+      // vertical sync polarity
+      const vPolarity = !params.redBlnk
+
+
+      return {
+        links: links,
+        freq: freq.toFixed(3),
+
+        hTotal: hTotal,
+        hFrontPorch: hFrontPorch,
+        hActive: hPxRounded,
+        hSync: hSyncRounded,
+        hPolarity: hPolarity,
+  
+        vTotal: vTotal,
+        vFrontPorch: vFrontPorch,
+        vActive: vLines,
+        vPolarity: vPolarity,
+        vSync: vSync,
+        vRate: params.refresh
+      }
+    }
+  }
+
   /**
    * Returns connector capacity as a string
    * @param {number} hPx - active horizontal pixels (px)
@@ -100,12 +251,12 @@ class edid {
 
   /**
    * Returns the VESA aspect ratio as a string
-   * @param {number} hPix - horizontal pixels
-   * @param {number} vPix - vertical pixels
-   * @returns {string}
+   * @param {number} hPx - horizontal pixels
+   * @param {number} vPx - vertical pixels
+   * @returns {string} string value, *e.g. '4:3'*
    */
-  aspectRatio(hPix, vPix) {
-    switch (hPix / vPix) {
+  aspectRatio(hPx, vPx) {
+    switch (hPx / vPx) {
       case 4/3:
         return '4:3'
         break;
--- test/edid.test.js
+++ test/edid.test.js
@@ -1,42 +1,45 @@
-const assert = require('assert');
+const assert = require('assert')
 const edid = require('./../src/lib/edid')
 
 describe('Constuctor', () => {
   const constEdid = new edid(1920,1080,60,false,true,2)
 
-  it('should set params', () => {
+  it('set params', () => {
     assert.equal(constEdid.params.hPx, 1920)
-    assert.equal(constEdid.params.vPx, 1080);
-    assert.equal(constEdid.params.refresh, 60);
-    assert.equal(constEdid.params.margins, false);
-    assert.equal(constEdid.params.redBlnk, true);
-    assert.equal(constEdid.params.redBlnkV, 2);
-  });
-});
+    assert.equal(constEdid.params.vPx, 1080)
+    assert.equal(constEdid.params.refresh, 60)
+    assert.equal(constEdid.params.margins, false)
+    assert.equal(constEdid.params.redBlnk, true)
+    assert.equal(constEdid.params.redBlnkV, 2)
+  })
+})
 
 describe('Calculate EDID', () => {
   it('1920x1080@60 // no margins // no reduced blanking', () => {
-    const calcEdid = new edid(1920,1080,60,false,false,2)
+    const calcEdid = new edid(1920,1080,60,false,false,1)
 
-    var result = calcEdid.calculateEdid()
+    var result = calcEdid.calcEdid()
+
+    assert.equal(result.links, 'DL')
+
+    assert.equal(result.freq, 173.101)
+
+    assert.equal(result.hTotal, 2576)
+    assert.equal(result.hFrontPorch, 128)
+    assert.equal(result.hActive, 1920)
+    assert.equal(result.hSync, 200)
+    assert.equal(result.hPolarity, false)
+
+    assert.equal(result.vTotal, 1120)
+    assert.equal(result.vFrontPorch, 3)
+    assert.equal(result.vActive, 1080)
+    assert.equal(result.vPolarity, true)
+    assert.equal(result.vSync, 5)
+    assert.equal(result.vRate, 60)
     
-    console.log(result)
+  })
+})
 
-    assert.equal(result.hTotalPx, 2578);
-    assert.equal(result.hFrontPorch, 123);
-    assert.equal(result.hActive, 1920);
-    assert.equal(result.hSync, 206);
-    assert.equal(result.hPolarity, false);
-
-    assert.equal(result.vTotalLines, 1120);
-    assert.equal(result.vFrontPorch, 3);
-    assert.equal(result.vActive, 1080);
-    assert.equal(result.vPolarity, true);
-    assert.equal(result.vSync, 5);
-    assert.equal(result.vRate, 60);
-    
-  });
-});
 describe('Link Capacity', () => {
   const linkEdid = new edid(1920,1080,60,true,2)
 
@@ -81,18 +84,18 @@ describe('Aspect Ratio', () => {
   const aspectEdid = new edid(1920,1080,60,true,2)
 
   it('1024x768 => 4:3', () => {
-    assert.equal(aspectEdid.aspectRatio(1024,768), '4:3');
-  });
+    assert.equal(aspectEdid.aspectRatio(1024,768), '4:3')
+  })
   it('1920x1080 => 16:9', () => {
-    assert.equal(aspectEdid.aspectRatio(1920,1080), '16:9');
-  });
+    assert.equal(aspectEdid.aspectRatio(1920,1080), '16:9')
+  })
   it('1920x1200 => 16:10', () => {
-    assert.equal(aspectEdid.aspectRatio(1920,1200), '16:10');
-  });
+    assert.equal(aspectEdid.aspectRatio(1920,1200), '16:10')
+  })
   it('1280x1024 => 5:4', () => {
-    assert.equal(aspectEdid.aspectRatio(1280,1024), '5:4');
-  });
+    assert.equal(aspectEdid.aspectRatio(1280,1024), '5:4')
+  })
   it('1500x900 => 15:9', () => {
-    assert.equal(aspectEdid.aspectRatio(1500,900), '15:9');
-  });
-});
+    assert.equal(aspectEdid.aspectRatio(1500,900), '15:9')
+  })
+})
